76f5aa5b160fb3ef35b9925ab4682b884bfc86d4
Davit Karamyan <dkaramyan@nvidia.com>
2025-03-29 15:08:16 +0400

    sync, async, threaded image downloader

--- asyncio/img_download.py
+++ asyncio/img_download.py
@@ -3,6 +3,7 @@ import requests
 import aiohttp
 import aiofiles
 import os
+import threading
 
 from time import time
 
@@ -38,12 +39,32 @@ def download_image_sync(url, filename="image.jpg"):
     print(f"Image saved as {filename}")
 
 
+
+N_REQUESTS = 100
+
 def download_files_sync(folder):
     URL  = "https://loremflickr.com/cache/resized/defaultImage.small_1000_1000_nofilter.jpg"
     os.makedirs(folder, exist_ok=True)
-    for i in range(10):
+    for i in range(N_REQUESTS):
         filename =  os.path.join(folder, f"image_{i}.jpg")  
         download_image_sync(URL, filename)
+    print("All images downloaded successfully")
+
+
+def download_files_threads(folder):
+    URL  = "https://loremflickr.com/cache/resized/defaultImage.small_1000_1000_nofilter.jpg"
+    os.makedirs(folder, exist_ok=True)
+    
+    threads = []
+    for i in range(N_REQUESTS):
+        filename =  os.path.join(folder, f"image_{i}.jpg")  
+        thread = threading.Thread(target=download_image_sync, args=(URL, filename))
+        thread.start()
+        threads.append(thread)
+    
+    # Wait for all threads to finish
+    for thread in threads:
+        thread.join()
     print("All images downloaded successfully")
 
 
@@ -51,7 +72,7 @@ async def download_files_async(folder):
     URL  = "https://loremflickr.com/cache/resized/defaultImage.small_1000_1000_nofilter.jpg"
     os.makedirs(folder, exist_ok=True)
     tasks = []
-    for i in range(10):
+    for i in range(N_REQUESTS):
         filename =  os.path.join(folder, f"image_{i}.jpg")  
         tasks.append(download_image_async(URL, filename))
     await asyncio.gather(*tasks)
@@ -66,6 +87,11 @@ if __name__ == "__main__":
     exec_time = time() - start
     print(f"Execution time for synchronous download: {exec_time:.2f} seconds")
 
+    # Threaded download
+    start = time()
+    download_files_threads("images_threads")
+    exec_time = time() - start
+    print(f"Execution time for threaded download: {exec_time:.2f} seconds")
 
     # Asynchronous download
     start = time()
